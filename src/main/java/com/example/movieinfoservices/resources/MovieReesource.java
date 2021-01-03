13a705f55b3ca6ad3b190371af20c19fa4a39c46
LakshyaRepo <sharma.lakshya5@gmail.com>
2021-01-03 12:47:08 +0530

    Initial commit

---
+++ src/main/java/com/example/movieinfoservices/resources/MovieReesource.java
@@ -0,0 +1,15 @@
+package com.example.movieinfoservices.resources;
+
+import com.example.movieinfoservices.models.Movie;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieReesource {
+    @RequestMapping("/{movieId}")
+    public Movie getMovieInfo(@PathVariable("movieId") String movieId){
+         return new Movie(movieId,"Naruto" );
+    }
+}
